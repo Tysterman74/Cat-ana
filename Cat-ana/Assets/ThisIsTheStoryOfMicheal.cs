1b45d596cc0d53414a60c357e0886c301d6c4def
josharae <josharaedwards@yahoo.com>
2015-01-30 10:28:33 -0800

    Added a script mmmmm.

---
+++ Cat-ana/Assets/ThisIsTheStoryOfMicheal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThisIsTheStoryOfMicheal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //this is the story of micheal 
+        //the ugliest boy in town
+        //ugly and weak
+        //they called him a freak
+        //so he lived in a hole underground
+        //he lived in a hole underground
+        //he lived on his own underground....
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
