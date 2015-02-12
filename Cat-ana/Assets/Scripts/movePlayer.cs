f25bbcf7568dcb99a673af035087d5758d82f7b1
holmes1309 <alexbagg03@gmail.com>
2015-02-11 18:12:32 -0800

    Implemented player movements (running/jumping) and added a test prefab.

---
+++ Cat-ana/Assets/Scripts/movePlayer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlayer : MonoBehaviour 
+{	
+	public GameObject groundCheck;
+	public LayerMask whatIsGround;
+
+	// Key inputs
+	public KeyCode right;
+	public KeyCode left;
+	public KeyCode up;
+
+	// Player properties
+	public float speed = 5.0f;
+
+
+	// Player jumping properties
+	public float jumpSpeed = 600.0f;
+	private float groundHeight; 
+
+
+	void FixedUpdate () 
+	{
+		bool isGround = Physics2D.OverlapCircle(groundCheck.transform.position, 0.03f, whatIsGround);
+
+		// Right arrow key pressed?
+		if (Input.GetKey(right)) 
+		{
+			rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+		}
+		
+		// Down arrow key pressed?
+		else if (Input.GetKey(left)) 
+		{
+			rigidbody2D.velocity = new Vector2(-speed, rigidbody2D.velocity.y);
+		}
+
+		else
+		{
+			rigidbody2D.velocity = new Vector2(0.0f, rigidbody2D.velocity.y);
+		}
+
+
+		// Up arrow key (jump) pressed once?
+		if(isGround)
+		{
+			if (Input.GetKeyDown(up)) 
+			{
+				rigidbody2D.AddForce(new Vector2(0.0f, jumpSpeed));
+			}
+		}
+	}
+}
