bddb4a750931a4f8d8a454b224f484a3a8491182
jwardlaw <jwardlaw@uci.edu>
2015-02-13 15:45:57 -0800

    Put in a line renderer to make it simple to test raycast.

--- Cat-ana/Assets/Scripts/EnemyTracking.cs
+++ Cat-ana/Assets/Scripts/EnemyTracking.cs
@@ -11,6 +11,7 @@ public class EnemyTracking : MonoBehaviour {
 
     private bool facingRight;
     private Vector2 direction;
+    private LineRenderer line;
 	// Use this for initialization
 	void Start () {
 	    //Find the children of this gameobject, which is Endpoint1 and Endpoint2
@@ -19,15 +20,27 @@ public class EnemyTracking : MonoBehaviour {
         player = transform.FindChild("Enemy").gameObject;
         facingRight = true;
         direction = Vector2.right;
+        line = player.GetComponent<LineRenderer>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        Vector3 directionVector =  new Vector3(viewDistance, 0.0f);
+        line.SetPosition(0, player.transform.position);
         if (facingRight)
+        {
             direction = Vector2.right;
+            line.SetPosition(1, (directionVector + player.transform.position));
+        }
         else
+        {
             direction = -Vector2.right;
+            line.SetPosition(1, -(directionVector + player.transform.position));
+        }
         RaycastHit2D hit = Physics2D.Raycast(player.transform.position, direction, viewDistance);
+
+        player.rigidbody2D.velocity = new Vector2((direction.x * 5.0f) , player.rigidbody2D.velocity.y);
+
         //move to endpoint 2...when hit endpoint 2 face the other direction
         //Move to endpoint 1.
     }
