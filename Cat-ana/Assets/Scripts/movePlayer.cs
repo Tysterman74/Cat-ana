4e1b235ab9d53712d917e25a2b5de0a1e84ac2b1
jwardlaw <jwardlaw@uci.edu>
2015-02-13 17:50:29 -0800

    Changed the color of player to all black when hidden. Temp change once art is in.

--- Cat-ana/Assets/Scripts/movePlayer.cs
+++ Cat-ana/Assets/Scripts/movePlayer.cs
@@ -19,40 +19,61 @@ public class movePlayer : MonoBehaviour
 	public float jumpSpeed = 600.0f;
 	private float groundHeight;
 
+    private SpriteRenderer renderPlayer;
     private bool hiding = false;
 
     void Start()
     {
         groundCheck = transform.FindChild("GroundCheck").gameObject;
+        renderPlayer = GetComponent<SpriteRenderer>();
     }
 
 	void FixedUpdate () 
 	{
-		if (hiding == false) {
-						bool isGround = Physics2D.OverlapCircle (groundCheck.transform.position, 0.03f, whatIsGround);
-			
-						// Right arrow key pressed?
-						if (Input.GetKey (right)) {
-								rigidbody2D.velocity = new Vector2 (speed, rigidbody2D.velocity.y);
-						}
-			
-			// Down arrow key pressed?
-			else if (Input.GetKey (left)) {
-								rigidbody2D.velocity = new Vector2 (-speed, rigidbody2D.velocity.y);
-						} else {
-								rigidbody2D.velocity = new Vector2 (0.0f, rigidbody2D.velocity.y);
-						}
-			
-			
-						// Up arrow key (jump) pressed once?
-						if (isGround) {
-								if (Input.GetKeyDown (up)) {
-										rigidbody2D.AddForce (new Vector2 (0.0f, jumpSpeed));
-								}
-			}
-				}
+        if (hiding == false)
+        {
+            bool isGround = Physics2D.OverlapCircle(groundCheck.transform.position, 0.03f, whatIsGround);
+
+            // Right arrow key pressed?
+            if (Input.GetKey(right))
+            {
+                rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+            }
+
+            // Down arrow key pressed?
+            else if (Input.GetKey(left))
+            {
+                rigidbody2D.velocity = new Vector2(-speed, rigidbody2D.velocity.y);
+            }
+            else
+            {
+                rigidbody2D.velocity = new Vector2(0.0f, rigidbody2D.velocity.y);
+            }
+
+
+            // Up arrow key (jump) pressed once?
+            if (isGround)
+            {
+                if (Input.GetKeyDown(up))
+                {
+                    rigidbody2D.AddForce(new Vector2(0.0f, jumpSpeed));
+                }
+            }
+        }
 
 	}
+
+    void Update()
+    {
+        if (!hiding)
+        {
+            renderPlayer.color = new Color(1f, 1f, 1f, 1f);
+        }
+        else
+        {
+            renderPlayer.color = new Color(0f, 0f, 0f, 1f);
+        }
+    }
 
     void setHiding()
     {
