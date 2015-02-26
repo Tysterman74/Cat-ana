dd770b45fc868ffa668c16e1fc12207525b1f6cc
Pafoua <psfang@uci.edu>
2015-02-25 18:16:34 -0800

    Yarnball is able to be thrown.

--- Cat-ana/Assets/Scripts/movePlayer.cs
+++ Cat-ana/Assets/Scripts/movePlayer.cs
@@ -6,11 +6,15 @@ public class movePlayer : MonoBehaviour
     private ParticleSystem smoke;
 	private GameObject groundCheck;
 	public LayerMask whatIsGround;
+    private int charge;
+    public int maxcharge;
+    private GameObject yarnball;
 
 	// Key inputs
 	public KeyCode right;
 	public KeyCode left;
 	public KeyCode up;
+    public KeyCode yarn;
 
 	// Player properties
 	public float speed = 5.0f;
@@ -23,11 +27,20 @@ public class movePlayer : MonoBehaviour
     private SpriteRenderer renderPlayer;
     private bool hiding = false;
 
+    IEnumerator resetCollider()
+    {
+        yield return new WaitForSeconds(1.0f);
+        rigidbody2D.gravityScale = 1.5f;
+        collider2D.isTrigger = false;
+    }
+
     void Start()
     {
         groundCheck = transform.FindChild("GroundCheck").gameObject;
         smoke = transform.FindChild("Smoke").GetComponent<ParticleSystem>();
         renderPlayer = GetComponent<SpriteRenderer>();
+        charge = 0;
+        yarnball = transform.FindChild("YarnBall").gameObject;
     }
 
 	void FixedUpdate () 
@@ -59,8 +72,36 @@ public class movePlayer : MonoBehaviour
                 if (Input.GetKeyDown(up))
                 {
                     rigidbody2D.AddForce(new Vector2(0.0f, jumpSpeed));
+
                 }
+                if (Input.GetKey(yarn))
+                {
+                    print("charge");
+         
+                    charge += 1;
+                    if (charge > 100)
+                    {
+                        charge = 100;
+                    
+                    }
+                
+                }
+                if (Input.GetKeyUp(yarn))
+                {
+                    print("thrown");
+                    print(charge);
+                    rigidbody2D.gravityScale = 0.0f;
+                    collider2D.isTrigger = true;
+                    yarnball.transform.parent = null;
+                    yarnball.rigidbody2D.AddForce(new Vector2(charge*10,0));
+                    StartCoroutine(resetCollider());
+                
+                }
+               
+
             }
+
+
         }
 
 	}
