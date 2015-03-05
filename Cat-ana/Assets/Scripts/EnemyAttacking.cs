bef852f87040fb99a617245e88f70c56e474b026
holmes1309 <alexbagg03@gmail.com>
2015-03-04 18:57:38 -0800

    Added a win con and lose/reset

---
+++ Cat-ana/Assets/Scripts/EnemyAttacking.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttacking : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+            Application.LoadLevel("playerMovement3");
+    }
+}
