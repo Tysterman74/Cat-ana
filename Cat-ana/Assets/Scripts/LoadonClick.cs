4a3c64f7d6ffd4b93c42495ddb2b14daaed29b79
Pafoua <psfang@uci.edu>
2015-05-20 17:16:45 -0700

    HUBWORLD

---
+++ Cat-ana/Assets/Scripts/LoadonClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LoadonClick : MonoBehaviour
+{
+    public void LoadScene(int level)
+    {
+            Application.LoadLevel(level);
+    
+    }
+
+}
