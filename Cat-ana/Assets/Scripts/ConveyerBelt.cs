1d3a8ef7897a9d751cc780e569813beec93545f2
holmes1309 <alexbagg03@gmail.com>
2015-05-20 17:19:42 -0700

    null

--- Cat-ana/Assets/Scripts/ConveyerBelt.cs
+++ Cat-ana/Assets/Scripts/ConveyerBelt.cs
@@ -10,15 +10,19 @@ public class ConveyerBelt : MonoBehaviour {
     private bool ballOnConveyerBelt = false;
     private GameObject player;
     private GameObject yarn;
+    private GameObject flower;
     private movePlayer playerComponent;
     private Yarnball yarnComponent;
+    private Flower flowerComponent;
+   
     // Use this for initialization
 	void Start () {
         player = GameObject.Find("Player");
         playerComponent = player.GetComponent<movePlayer>();
         yarn = GameObject.Find("YarnBall");
         yarnComponent = yarn.GetComponent<Yarnball>();
-
+        flower = GameObject.Find("Flower");
+        flowerComponent.GetComponent<Flower>();
     }
 	
 	// Update is called once per frame
@@ -35,10 +39,14 @@ public class ConveyerBelt : MonoBehaviour {
         }
         else if (col.gameObject.tag == "YarnBall")
         {
-            print("SADFDSA");
             yarnComponent.setExternalVelocity(new Vector2(left ? -speed : speed, 0));
             //col.gameObject.SendMessage("setExternalVelocity", new Vector2(left ? -speed : speed, 0));
             ballOnConveyerBelt = true;
+        }
+        else if (col.gameObject.tag == "Flower")
+        {
+            print("SADFDSA");
+            flower.GetComponent<Rigidbody2D>().velocity = new Vector2(left ? -speed : speed, 0);
         }
     }
 
@@ -52,6 +60,10 @@ public class ConveyerBelt : MonoBehaviour {
         {
             yarnComponent.setExternalVelocity(new Vector2(0, 0));
         }
+        else if (col.gameObject.tag == "Flower")
+        {
+            flower.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D col) 
@@ -60,6 +72,9 @@ public class ConveyerBelt : MonoBehaviour {
             collide = true;
 
         else if (col.gameObject.tag == "YarnBall")
+            collide = true;
+
+        else if (col.gameObject.tag == "Flower")
             collide = true;
     }
 
